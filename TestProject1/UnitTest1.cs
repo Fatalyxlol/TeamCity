186eacb7d3754325905791a9a1d98a37c126fae4
Fatalyxlol <nickshel@yandex.ru>
2023-12-18 21:29:10 +0300

    added tests

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, 2);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(3,3);
+        }
+    }
+}
